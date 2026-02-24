bc99f128b8d1bedda011b30a7c7a6765a8bf3ac4
sandovad8365 <sandovad8365@student.faytechcc.edu>
2026-02-24 00:41:43 -0500

    Add P1LAB1 assignment file

---
+++ P1LAB1_SandovalDarrin.py
@@ -0,0 +1,12 @@
+# Darrin Sandoval
+# February 22, 2026
+# P1LAB1 - Input and Output Basics
+# This program asks the user for their first and last name separately,
+# then displays a personalized welcome message to the CTI110 course.
+
+# Get input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Welcome to CTI110,", first_name, last_name + "!")
